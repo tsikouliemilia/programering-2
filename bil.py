01229dafeef17677f170136324014af539c68038
tsikouliemilia <emilia.tsikouli@elev.ga.ntig.se>
2026-09-01 10:04:41 +0200

    first commit

---
+++ bil.py
@@ -0,0 +1,21 @@
+class bil():
+    def __init__(self):
+        self.registreringsnummer = ""
+        self.fabrikat = ""
+        self.årsmodell = ""
+        self.motorefekt = ""
+
+bil_minukuper_d = bil()
+bil_volswagen_tiguan= bil ()
+
+
+bil_minukuper_d.registreringsnummer = "MFD160"
+bil_minukuper_d.fabrikat="mini bmw"
+bil_minukuper_d.årsmodell = 16
+bil_minukuper_d.motorefekt = "116hk"
+
+
+bil_volswagen_tiguan.registreringsnummer = "SJJ681"
+bil_volswagen_tiguan.fabrikat=""
+bil_volswagen_tiguan.årsmodell = 16
+bil_volswagen_tiguan.motorefekt = "116hk"
