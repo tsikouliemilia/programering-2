ed23b7a3ac6d34f79fa6e58880f111394e91f4c2
tsikouliemilia <emilia.tsikouli@elev.ga.ntig.se>
2026-09-03 10:51:22 +0200

    hbhbh

---
+++ prg2_uppgift1/tärningsduell.py
@@ -0,0 +1,16 @@
+from random import randint
+
+class spelare:
+    def __init__(self):
+        self.namn = ""
+        self.point = ""
+        
+    
+spelare1 = spelare ()
+spelare1.namn = ("rei")
+spelare1.point = ""
+
+spelare2 = spelare()
+spelare2.namn = "marina"
+spelare2.point =""
+
---
+++ test.py
@@ -0,0 +1,10 @@
+class person():
+    def __init__(self):
+        self.förnamn = ""
+        self.efternamn = ""
+        self.födelseår = ""
+        self.singel = True
+
+person1 = person ()
+person1.förnamn = "Emilia"
+person1.efternamn = "tsikouli"
